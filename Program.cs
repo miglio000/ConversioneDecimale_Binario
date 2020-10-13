51e2a0c51000e12e1a9ceb8a297bff5366e970ab
miglio000 <gabrielemiglio23@gmail.com>
2020-10-13 09:21:07 +0200

    prima versione dell'esercizione che convere da decimale a binario

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace ConversioneDecimale_Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num, resto;
+            Console.WriteLine("Inserire il numero da convertire:");
+            num = Convert.ToInt32(Console.ReadLine));
+            string binario = "";
+            while (num 0){
+                resto = num % 2;
+                num /= 2;
+                binario = Convert.ToString(resto) + binario;
+
+            }
+            Console.WriteLine($"Il numero")
+
+
+
+            
+            
+        }
+    }
+}
