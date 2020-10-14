8c3b625b9d566e5dee1ca002d2006f42d81cd5bd
miglio000 <gabrielemiglio23@gmail.com>
2020-10-14 09:17:42 +0200

    da finire di corregere da decimale a binario,ottale e esadecimale.

--- Program.cs
+++ Program.cs
@@ -8,18 +8,25 @@ namespace ConversioneDecimale_Binario
         static void Main(string[] args)
         {
 
-            int num, resto;
+            int num, resto, resto8, resto16, temp;
             Console.WriteLine("Inserire il numero da convertire:");
-            num = int.Parse(Console.ReadLine));
+            num = int.Parse(Console.ReadLine());
             string binario = "";
+            string ottale = "";
+            string esadecimale = "";
             while (num > 0)
-                    {
+            {
                 resto = num % 2;
                 num /= 2;
                 binario = Convert.ToString(resto) + binario;
-
             }
+            num = temp;
+
+            
             Console.WriteLine($"Il numero in binario è {binario}");
+            Console.WriteLine($"Il numero in ottale è {ottale}");
+            Console.WriteLine($"Il numero in esadecimale è {esadecimale}");
+            Console.ReadLine();
 
 
 
