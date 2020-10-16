1d4e7751e4f508aee574946b24bf5ecf41b684ff
miglio000 <gabrielemiglio23@gmail.com>
2020-10-16 12:20:55 +0200

    Conversione da decimale a tutte le basi tra 2 e 16.

--- Program.cs
+++ Program.cs
@@ -8,31 +8,53 @@ namespace ConversioneDecimale_Binario
         static void Main(string[] args)
         {
 
-            int num, resto, resto8, resto16, temp;
-            Console.WriteLine("Inserire il numero da convertire:");
-            num = int.Parse(Console.ReadLine());
-            string binario = "";
-            string ottale = "";
-            string esadecimale = "";
-            while (num > 0)
+            int num, resto, valorebase, temp;
+            string numconvertito = "";
+            Console.WriteLine("Inserire un numero da convertire:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserire la base in cui convertire il numero (basi da 2 a 16):");
+            valorebase = Convert.ToInt32(Console.ReadLine());
+            temp = num;
+            if (valorebase > 16 || valorebase < 2)
             {
-                resto = num % 2;
-                num /= 2;
-                binario = Convert.ToString(resto) + binario;
+                Console.WriteLine("Non puoi convertire in basi sopra la esadecimale.");
+                Console.WriteLine("Non puoi convertire in una base minore della binaria.");
             }
-            num = temp;
-
-            
-            Console.WriteLine($"Il numero in binario è {binario}");
-            Console.WriteLine($"Il numero in ottale è {ottale}");
-            Console.WriteLine($"Il numero in esadecimale è {esadecimale}");
+            else
+            {
+                while (num > 0)
+                {
+                    resto = num % valorebase;
+                    num /= valorebase;
+                    switch (resto)
+                    {
+                        case 10:
+                            numconvertito = "A";
+                            break;
+                        case 11:
+                            numconvertito = "B";
+                            break;
+                        case 12:
+                            numconvertito = "C";
+                            break;
+                        case 13:
+                            numconvertito = "D";
+                            break;
+                        case 14:
+                            numconvertito = "E";
+                            break;
+                        case 15:
+                            numconvertito = "F";
+                            break;
+                        default:
+                            numconvertito = Convert.ToString(resto) + numconvertito;
+                            break;
+                    }
+                }
+                Console.WriteLine($"Il numero {temp} in base {valorebase} è: {numconvertito}.");
+            }
             Console.ReadLine();
-
-
-
-
-
-
         }
     }
 }
+
